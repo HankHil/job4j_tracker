534d356c4bd09e35ad01ebf661f9be5403892cc7
HankHil <xtorinx@yandex.ru>
2020-06-18 03:51:30 +0300

    1

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        String say = "Неизвестное слово. " + eng;
+        return say;
+    }
+
+    public static void main(String[] args) {
+        DummyDic word = new DummyDic();
+        System.out.println(word.engToRus("goal"));
+    }
+}
